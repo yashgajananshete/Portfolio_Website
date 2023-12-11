828cc9213515a173be8197ee837c068b3e4aedac
YASH GAJANAN SHETE <yashshete22@gmail.com>
2023-12-11 17:28:54 +0530

    change1

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homee from './routes/Homee';
-import About from './routes/About';
-import Contact from './routes/Contact';
-import Projeect from './routes/Projeect';
-
-function App() {
-  return (
-    <BrowserRouter basename="/Portfolio_Website">
-      <Routes>
-        <Route path="/" element={<Homee />} />
-        <Route path="/project" element={<Projeect />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
-
-
-
-// import Homee from "./routes/Homee";
-// import About from "./routes/About";
-// import Contact from "./routes/Contact";
-// import Projeect from "./routes/Projeect";
-
-// import { Routes, Route } from "react-router-dom";
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Homee from './routes/Homee';
+// import About from './routes/About';
+// import Contact from './routes/Contact';
+// import Projeect from './routes/Projeect';
 
 // function App() {
 //   return (
-//     <>
-//     {/* <BrowserRouter basename="/Portfolio_Website"> */}
+//     <BrowserRouter basename="/Portfolio_Website">
 //       <Routes>
 //         <Route path="/" element={<Homee />} />
 //         <Route path="/project" element={<Projeect />} />
 //         <Route path="/about" element={<About />} />
 //         <Route path="/contact" element={<Contact />} />
 //       </Routes>
-//       {/* </BrowserRouter> */}
-//     </>
+//     </BrowserRouter>
 //   );
 // }
 
 // export default App;
+
+
+
+
+import Homee from "./routes/Homee";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Projeect from "./routes/Projeect";
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    {/* <BrowserRouter basename="/Portfolio_Website"> */}
+      <Routes>
+        <Route path="/" element={<Homee />} />
+        <Route path="/project" element={<Projeect />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/* </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
