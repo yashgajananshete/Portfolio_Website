5e7e74b9c1511bad8aaac65576e7d64a736d747d
Yash Gajanan Shete <yashshete22@gmail.com>
2024-01-12 02:36:26 +0530

    Update Aboutmee.js

--- src/Components/Aboutmee.js
+++ src/Components/Aboutmee.js
@@ -8,13 +8,19 @@ export const Aboutmee = () => {
         <p id="aboutp">
 
 
-        <p>Hello, <b>I'm Yash Gajanan Shete</b>, born and brought up in Amravati, Maharashtra. I recently graduated from Government College of Engineering, Amravati, and I am currently pursuing an internship at Cognizant Technology Solutions Pvt. Ltd. in the Cognizant Infrastructure Services domain.</p>
-        <p>My technical skills include proficiency in <b>C, C++, Java, SQL, HTML, CSS,</b> and intermediate knowledge of <b>JavaScript, React.js, JDBC, Python, TypeScript,</b> and basic knowledge of <b>Node.js</b>. In addition to my mechanical engineering background, I also hold certification in <b>Catia design software</b>.</p>
-        <p>One of my significant achievements is the completion of various personal coding projects that can be found on my Projects page. Additionally, I was involved in a college project,<b> 'Design and Fabrication of Rocker Bogie Mechanism,'</b> which is designed to assist farmers in optimizing farming processes, saving time and effort.</p>
-        <p>I am known for my strong work ethic, discipline, dedication, and problem-solving abilities. As I pursue a career in the IT sector as a developer, I aim to maintain a healthy work-life balance and continue to develop my skills.</p>
-        <p>I've had leadership experiences as the captain of my department's cricket team in my final year, where we reached the semifinals. My journey of living away from my family since childhood has instilled in me responsibility and maturity at an early age.</p>
-        <p>My career goal is to excel in the IT sector and make a meaningful contribution as a developer while achieving a harmonious balance between my professional and personal life.</p>
-          </p>
+<p>Hello, <b>I'm Yash Gajanan Shete</b>, born and raised in Amravati, Maharashtra. I recently graduated with a B.Tech degree from Government College of Engineering, Amravati. I successfully completed an internship at Cognizant Technology Solutions Pvt. Ltd, focusing on the CIS DWP domain with emphasis on Communication and Voice & Accent from August 2023 to October 2023.</p>
+
+    <p>My technical skills include proficiency in <b>Java, C, C++, SQL, HTML, CSS, JavaScript </b>, and intermediate knowledge of <b>React.js, JDBC, Python, Node.js, Express.js</b>. I recently completed my backend development, gaining expertise in <b>Servlet, JSP, JDBC, Hibernate, Spring Core, Spring JDBC, Spring ORM, Spring MVC, Spring Boot</b>. In addition to my mechanical engineering background, I also hold certification in <b>Catia design software</b>.</p>
+
+    <p>One of my noteworthy achievements is successfully completing various personal coding projects, detailed on my Projects page. Additionally, I actively contributed to a college project, <b>'Design and Fabrication of Rocker Bogie Mechanism,'</b> designed to optimize farming processes, thereby saving time and effort for farmers.</p>
+
+    <p>I am recognized for my strong work ethic, discipline, dedication, and problem-solving abilities. In my final year, I served as the captain of my department's cricket team, leading the team to the semifinals. Having lived away from my family since childhood, I have developed a sense of responsibility and maturity at an early age.</p>
+
+    <p>With my backend development expertise and a diverse skill set, I am eager to pursue a career in the IT sector as a developer. My career goal is to excel in the IT industry, making meaningful contributions while maintaining a harmonious balance between my professional and personal life.</p>
+
+    <p>Feel free to check my GitHub page for more details and to explore my projects: <a href="https://github.com/yashgajananshete">GitHub</a>.</p>
+
+    <p>Connect with me on <a href="https://www.linkedin.com/in/yash-shete-721a791ba">LinkedIn</a> for further discussions or opportunities.</p
        
     </div>
   )
